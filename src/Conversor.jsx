7ef12ccab330b89a9ad310d7807f12d136cc7261
jonathanperez13 <jp911309@gmail.com>
2024-08-01 09:14:51 -0500

    "loguin y Conversor de texto a voz y voz a texto"

---
+++ src/Conversor.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function conversor() {
+  const [textoAVoz, setTextoAVoz] =useState('')
+  const [vozATexto, setVozATexto] =useState('')
+}
+  function cambiarTexto(evento) {
+    setTextoAVoz(evento.target.value)
+  }
+  function convertirTextoAVoz() {
+    const synth = window.speechSynthesis
+    const utterThi8s = new SpeechSynthesisUtterance(textoAVoz)
+    synth.speak(utterThis)
+  }
+  function resultado(event) {
+    setVozATexto(event.onresult[0][0].transcript)
+  }
+
+  function grabarVozATexto() {
+    const recognition = new window.webkitSpeechRecognition()
+    recognition.lang = 'es-ES'
+    recognition.star()
+    recognition.onresult = resultado
+      
+  }
+  
+    return (
+      <>
+      <h1>Conversor TTS y STT</h1>
+      <br />
+    <h3>Conversor de texto a voz</h3>
+
+    <input type="text" nid="" id="textoAVoz" value={textoAVoz} onChange={cambiarTexto} />
+    <button onClick={convertirTextoAVoz}>Convertir</button>
+
+    <h3>Conversor de voz a texto</h3>
+    <button onClick={grabarVozATexto}>Grabar</button>
+    {vozATexto}
+      </>
+);
+
+
+export default conversor
